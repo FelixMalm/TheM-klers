ce2599564ff207f6bfcfd656af0cb899e4514183
FelixMalm <felix_malm98@hotmail.com>
2024-04-25 13:59:12 +0200

    Added images to add houseing

--- TheMäklersBlazor/Program.cs
+++ TheMäklersBlazor/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TheMäklersBlazor;
+using static TheMäklersBlazor.Pages.AddHouse;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7078/") });
+builder.Services.AddScoped<ILocalFileStorageService, LocalFileStorageService>();
 
 await builder.Build().RunAsync();
